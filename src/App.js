2273a1bfc951c0eacd01312a658ed68e318847e0
Tyler Haley <thaleycode@gmail.com>
2023-09-20 10:38:54 -0400

    nav bar work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LogIn from './components/LogIn';
-import NewUserSubmit from './NewUserSubmit';
-import Home from './home';
-import ForgotPassword from './forgotPassword';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LogIn from './pages/login/LogIn';
+import NewUserSubmit from './pages/newUserSubmit/NewUserSubmit';
+import Home from './pages/home/home';
+import ForgotPassword from './pages/forgotPassword/ForgotPassword';
+import ErrorPage from './pages/errorPage/ErrorPage';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/login"> Login </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
-        <Route path="*" element={<Error />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
