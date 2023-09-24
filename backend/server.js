d2d0a7e9fa1b813a6687bc70777a12d31a50e2d2
Tyler Haley <thaleycode@gmail.com>
2023-09-24 15:13:30 -0400

    login post

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const uri = require('./atlas_uri');
 const UserModel = require('./dataModels/UserInfo');
+const LogInModel = require('./dataModels/LogIn');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3001
@@ -23,12 +25,19 @@ mongoose.connect(uri, {
 app.get('/', (req, res) => res.status(200).send("Home Page"));
 
 //post to MongoDB
+
+//add user information when account is created
+app.post('/login/add', (req, res) => {
+    const loginInfo = req.body;
+
+    LogInModel.create(loginInfo);
+})
+
 app.post('/user/add', (req, res) => {
     const userInfo = req.body;
     console.log(`User Information>>>>>>>`, userInfo); 
 
     UserModel.create(userInfo);
-
 })
 
 app.listen(port, () => console.log("listening on port ", port));
