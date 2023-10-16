1129a6b21ff4cc15bf1b7ceef268083a2d3f8b2e
Tyler Haley <thaleycode@gmail.com>
2023-10-16 10:55:33 -0400

    init

---
+++ src/pages/notAllowed/NotAllowed.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotAllowed() {
+  return (
+    <div>
+        <h3>You are not authorized to access this page.</h3>
+        <h2>Please contact an administrator if you need assistence.</h2>
+    </div>
+  )
+};
+
+export default NotAllowed;
