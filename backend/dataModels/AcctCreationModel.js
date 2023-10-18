8346516d348bf18c8df85728da96a02d929ef913
Tyler Haley <thaleycode@gmail.com>
2023-10-18 09:37:53 -0400

    init

---
+++ backend/dataModels/AcctCreationModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const AcctCreationSchema = new mongoose.Schema({
+    name: String, 
+    number: String,
+    description: String,
+    normalSide: Number,
+    category: String,
+    subcategory: String,
+    initBalance: Number,
+    debit: Number,
+    credit: Number,
+    dateTimeAdded: Date,
+    createdBy: String,
+    order: Number,
+    statement: String
+    comment: String
+});
+
+const AcctCreationModel = mongoose.model("logIn", AcctCreationSchema);
+module.exports = AcctCreationModel;
