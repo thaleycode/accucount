b88ffd0ec9bbe1f57b59478642ca18216dabf08b
thaleycode <thaleycode@gmail.com>
2023-12-03 15:18:17 -0500

    form update, needs emailer

--- src/pages/formSubmitted/FormSubmitted.js
+++ src/pages/formSubmitted/FormSubmitted.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './FormSubmitted.css';
 
 function FormSubmitted() {
   return (
-    <div>
-        <h3>Your form has been submitted and is awaiting admin approval.</h3>
+    <div className="body">
+      <div className="header">Your form has been submitted and is awaiting admin approval.</div>
+      <div className="undertext">I was having trouble with the email system and couldn't get it to work. Currently all users are enabled by default as a user.</div>
     </div>
   )
 };
