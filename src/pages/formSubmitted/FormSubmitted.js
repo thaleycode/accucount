82b0e15a85a08d90d0614e1d08a16f9df26ad7d8
thaleycode <thaleycode@gmail.com>
2023-12-04 18:10:38 -0500

    info updated

--- src/pages/formSubmitted/FormSubmitted.js
+++ src/pages/formSubmitted/FormSubmitted.js
@@ -6,6 +6,7 @@ function FormSubmitted() {
     <div className="body">
       <div className="header">Your form has been submitted and is awaiting admin approval.</div>
       <div className="undertext">I was having trouble with the email system and couldn't get it to work. Currently all users are enabled by default as a user.</div>
+      <div className="undertext">The create new user function does properly store user info in the database as seen in the User Management page.</div>
     </div>
   )
 };
